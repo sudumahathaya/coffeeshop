26c9c3d02809fe9ec8624b272fc65f2a8a6efe41
Avindu Oshan <86357399+sudumahathaya@users.noreply.github.com>
2025-07-25 11:53:23 +0530

    Create Modern User Profile Page

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -469,13 +469,16 @@
                                 <li><a class="dropdown-item" href="{{ route('user.orders') }}">
                                         <i class="bi bi-receipt me-2"></i>My Orders
                                     </a></li>
+                                <li><a class="dropdown-item" href="{{ route('profile.view') }}">
+                                        <i class="bi bi-person me-2"></i>My Profile
+                                    </a></li>
                                 @if(Auth::user()->role === 'admin')
                                     <li><a class="dropdown-item" href="{{ route('admin.dashboard') }}">
                                             <i class="bi bi-gear me-2"></i>Admin Panel
                                         </a></li>
                                 @endif
                                 <li><a class="dropdown-item" href="{{ route('profile.edit') }}">
-                                        <i class="bi bi-person me-2"></i>Profile
+                                        <i class="bi bi-gear me-2"></i>Account Settings
                                     </a></li>
                                 <li>
                                     <hr class="dropdown-divider">
--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,9 @@ Route::get('/dashboard', function () {
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::get('/profile/view', function () {
+        return view('profile.index');
+    })->name('profile.view');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     
